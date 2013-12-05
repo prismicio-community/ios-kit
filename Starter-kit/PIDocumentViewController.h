55b96639210d9a1ec34f27811fe1a1a1c834ca32
Étienne Vallette d'Osia <evo@zenexity.com>
2013-12-05 11:45:30 +0100

    Add PIDocumentViewController

---
+++ Starter-kit/PIDocumentViewController.h
@@ -0,0 +1,15 @@
+//
+//  PIDocumentViewController.h
+//  Starter-kit
+//
+//  Created by Étienne VALLETTE d'OSIA on 04/12/2013.
+//  Copyright (c) 2013 Zengularity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PIAPI/PIAPI.h"
+
+@interface PIDocumentViewController : UIViewController
+
+@end
---
+++ Starter-kit/PIDocumentViewController.m
@@ -0,0 +1,38 @@
+//
+//  PIDocumentViewController.m
+//  Starter-kit
+//
+//  Created by Étienne VALLETTE d'OSIA on 04/12/2013.
+//  Copyright (c) 2013 Zengularity. All rights reserved.
+//
+
+#import "PIDocumentViewController.h"
+
+@interface PIDocumentViewController ()
+
+@end
+
+@implementation PIDocumentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
